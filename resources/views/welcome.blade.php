878072e70c3a6d4dc6418a31c9ea64ff14165860
vncorrea <vinicorreag2001@gmail.com>
2024-06-24 21:40:34 -0300

    build

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+/>
+    <title>Laravel</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+  </head>
+  <body class="antialiased">
+    <div id="header"></div>
+  </body>
+</html>
